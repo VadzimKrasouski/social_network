95f8f7ccf36e5e77935de1a99a85603440e24a23
VadzimKrasouski <vadzim.krasouski@gmail.com>
2020-04-03 19:35:55 +0200

    look much better

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import styles from './Profile.module.css'
-import wallpaper from '../../img/8788.png'
+import wallpaper from './../../img/8788.png'
+import avatar from './../../img/ava.jpeg'
 
 const Profile = () => {
     return (
-            <main className={styles.profile}>
-                <div>
-                    <img src={wallpaper} alt="wallpaper"/>
+        <div className={styles.profile}>
+            <div className={styles.wallpaper}>
+                <img src={wallpaper} alt="wallpaper"/>
+            </div>
+                <div className={styles.avatar}>
+                    <img src={avatar} alt="avatar"/>
                 </div>
-                <div>
-                    Profile
+                <div className={styles.descriptions}>
+                    Description
                 </div>
-            </main>
+            <div className={styles.posts}>
+                <div>My posts</div>
+                <div>New post</div>
+                <div>post1</div>
+                <div>post2</div>
+            </div>
+        </div>
     );
 };
 
