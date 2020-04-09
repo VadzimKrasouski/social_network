fd188665d88273988bc5c832e6106dfcd58157d7
VadzimKrasouski <vadzim.krasouski@gmail.com>
2020-04-09 14:28:37 +0200

    -rename navbar, -refactored Profile.jsx

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -5,11 +5,16 @@ const Sidebar = () => {
     return (
         <div className={styles.container}>
             <nav className={styles.nav}>
-                <div><a href="#">Profile</a></div>
-                <div><a href="#">Messages</a></div>
-                <div><a href="#">News</a></div>
-                <div><a href="#">Music</a></div>
-                <div><a href="#">Settings</a></div>
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                <div><a href="">Profile</a></div>
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                <div><a href="">Messages</a></div>
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                <div><a href="">News</a></div>
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                <div><a href="">Music</a></div>
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                <div><a href="">Settings</a></div>
             </nav>
         </div>
     );
