f9df907ecd6251f0f4ef6d184bdbba640fdaa651
VadzimKrasouski <vadzim.krasouski@gmail.com>
2020-04-24 09:18:12 +0200

    -refactored Cover.jsx

--- src/Components/Main/Profile/Cover/Cover.jsx
+++ src/Components/Main/Profile/Cover/Cover.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import styles from './Cover.module.css'
-import wallpaper from './8788.png'
 
 const Cover = () => {
     return (
         <div className={styles.cover}>
-            <img src={wallpaper} alt="wallpaper"/>
+            {/*<img src={wallpaper} alt="wallpaper"/>*/}
         </div>
     );
 };
