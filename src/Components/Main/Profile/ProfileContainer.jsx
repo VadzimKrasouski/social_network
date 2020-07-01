6d8846727f2258866c79563d768dc4786ed8ba19
VadzimKrasouski <vadzim.krasouski@gmail.com>
2020-07-01 23:49:07 +0200

    -profile router

--- src/Components/Main/Profile/ProfileContainer.jsx
+++ src/Components/Main/Profile/ProfileContainer.jsx
@@ -3,16 +3,21 @@ import {connect} from "react-redux";
 import {addPost, setUserProfile, updatePost} from "../../../Redux/profileReducer";
 import Profile from "./Profile";
 import * as axios from "axios";
+import {withRouter} from "react-router-dom";
 
 class ProfileContainer extends React.Component {
     componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`).then(response => {
+        let userId = this.props.match.params.userId
+
+        if (!userId) {
+            userId=2;
+        }
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/`+ userId).then(response => {
             this.props.setUserProfile(response.data);
         })
     }
 
     render() {
-        debugger
         return (
             <Profile {...this.props} profilePage={this.props.profilePage}/>
         );
@@ -20,5 +25,7 @@ class ProfileContainer extends React.Component {
 }
 
 const mapStateToProps = (state) => ({profilePage: state.profilePage});
-const ConnectedProfileContainer = connect(mapStateToProps, {addPost, updatePost, setUserProfile})(ProfileContainer);
+
+let WithUrlDataContainerComponent = withRouter(ProfileContainer);
+const ConnectedProfileContainer = connect(mapStateToProps, {addPost, updatePost, setUserProfile})(WithUrlDataContainerComponent);
 export default ConnectedProfileContainer;
